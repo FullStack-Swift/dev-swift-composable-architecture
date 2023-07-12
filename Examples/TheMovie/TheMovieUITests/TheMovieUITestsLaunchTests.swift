90cfe983f3953c521ca35766782bd8a9e45e9981
NguyenPhongVN <nguyenphong.mobile.engineer@gmail.com>
2023-07-12 14:35:25 +0700

    add TheMovie Example

---
+++ Examples/TheMovie/TheMovieUITests/TheMovieUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TheMovieUITestsLaunchTests.swift
+//  TheMovieUITests
+//
+//  Created by PhongND on 7/12/23.
+//
+
+import XCTest
+
+final class TheMovieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
