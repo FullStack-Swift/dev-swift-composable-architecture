230f2440c2ae5caac448185a541f6539142df8ad
NguyenPhongVN <nguyenphong.mobile.engineer@gmail.com>
2023-08-03 09:36:38 +0700

    fix isRecoil

--- Sources/ComposableArchitecture/Components/Recoil/IsRecoilValue.swift
+++ Sources/ComposableArchitecture/Components/Recoil/IsRecoilValue.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public func isRecoilType<T>(type: T) -> Bool {
+public func isRecoilType<Node>(type: Node) -> Bool {
   if type is (any Atom) {
     return true
   }
@@ -8,35 +8,35 @@ public func isRecoilType<T>(type: T) -> Bool {
 }
 
 
-public func isRecoilValue<V>(type: V) -> Bool {
+public func isRecoilValue<Node>(type: Node) -> Bool {
   if type is (any ValueAtom) {
     return true
   }
   return false
 }
 
-public func isRecoilState<T>(type: T) -> Bool {
+public func isRecoilState<Node>(type: Node) -> Bool {
   if type is (any StateAtom) {
     return true
   }
   return false
 }
 
-public func isRecoilPublisher<T>(type: T) -> Bool {
+public func isRecoilPublisher<Node>(type: Node) -> Bool {
   if type is (any PublisherAtom) {
     return true
   }
   return false
 }
 
-public func isRecoilTask<T>(type: T) -> Bool {
+public func isRecoilTask<Node>(type: Node) -> Bool {
   if type is (any TaskAtom) {
     return true
   }
   return false
 }
 
-public func isRecoilThrowingTask<T>(type: T) -> Bool {
+public func isRecoilThrowingTask<Node>(type: Node) -> Bool {
   if type is (any ThrowingTaskAtom) {
     return true
   }
