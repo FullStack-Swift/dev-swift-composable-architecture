5dd533952d1ed2643758e83710c898cdf3faf210
PhongND <phongnd@ftech.ai>
2023-11-02 15:51:42 +0700

    remove valueOnChanged

--- Sources/UIComponents/SwiftUIExt.swift
+++ Sources/UIComponents/SwiftUIExt.swift
@@ -138,21 +138,6 @@ extension View {
   }
 }
 
-// MARK: Changed Property
-extension View {
-  
-  @ViewBuilder
-  public func valueChanged<T: Equatable>(value: T, onChange: @escaping (T) -> Void) -> some View {
-    if #available(iOS 14.0, tvOS 14.0, macOS 11.0, watchOS 7.0, *) {
-      self.onChange(of: value, perform: onChange)
-    } else {
-      self.onReceive(Just(value)) { value in
-        onChange(value)
-      }
-    }
-  }
-}
-
 // MARK: onTap
 extension View {
   
